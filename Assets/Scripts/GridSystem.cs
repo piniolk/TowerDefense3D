06d16e6d50ab8e6e00326d22bc08f9ee445ff5e5
Katherine Piniol <piniolk@hawaii.edu>
2023-02-09 21:03:17 -1000

    Install gridbox prototype materials,

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem {
+    private int width, length, height = 10;
+
+    private void Start() {
+
+    }
+}
